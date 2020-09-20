85d2b4310361c60b49db1b1d250a4dfe08912157
Gustav Odinger <gustav@odinger.se>
2020-09-20 02:24:35 +0200

    Rewrite a lot of the emojify cog
    - Sends the output as a png; not text (emojis)
    
    - New tileset class for storing tileset related things, including emojis
    
    - Adds to_image and to_mapping methods to the tileset class
    
    - Stores a dictionary of tileset configurations in every emoji, so it'll be dynamic across servers
    
    - Adds load image method to the emoji class. This loads the emoji icon and stores it in ./tilesets/{tileset.name}/.
    
    - New file for constants, like emoji repo urls and a list of possible emojis
    
    - Updates .gitignore to exclude ./tilesets folder, where emoji pngs are stored
    
    - Moves emoji config files to under a guild specific directory. This allows the bot to support multiple servers

---
+++ bot/exts/evergreen/emojify/constants.py
@@ -0,0 +1,27 @@
+REPO_IMG_URLS = {
+    "beard": "https://raw.githubusercontent.com/gustavwilliam/branding/master/emotes/tilesets/lemoji_beard",
+    "snake": "https://raw.githubusercontent.com/gustavwilliam/branding/master/emotes/tilesets/pydis_snake",
+}
+
+EMOJIS = [
+    "blank",
+    "ht",
+    "hb",
+    "hl",
+    "hr",
+    "et",
+    "eb",
+    "el",
+    "er",
+    "trb",
+    "tr",
+    "tb",
+    "rb",
+    "lr",
+    "center",
+    "ltr",
+    "ltb",
+    "lt",
+    "lrb",
+    "lb"
+]
--- bot/exts/evergreen/emojify/emojify_cog.py
+++ bot/exts/evergreen/emojify/emojify_cog.py
@@ -1,14 +1,18 @@
+import io
 import json
 import logging
 import os
 import random
+import urllib.request
 from typing import Dict, List, Union
 
 import discord
-from discord import Colour, Embed, HTTPException
+from PIL import Image
+from discord import Colour, Embed, Guild, HTTPException
 from discord.ext.commands import BadArgument, Cog, CommandError, Context, MissingRequiredArgument, command
 
 from bot.constants import ERROR_REPLIES, NEGATIVE_REPLIES
+from bot.exts.evergreen.emojify.constants import EMOJIS, REPO_IMG_URLS
 
 log = logging.getLogger(__name__)
 
@@ -43,27 +47,98 @@ class Font:
 class Emoji:
     """Contains all data about an emoji."""
 
-    def __init__(self, name: str, guild: Context.guild):
-        self.name = name
-        self.guild = guild
+    def __init__(self, name: str):
+        self.name = name  # The emoji identifier used in fonts and files.
 
-    @property
-    def id(self) -> Union[int, None]:
+    def emoji_id(self, guild: Context.guild) -> Union[int, None]:
         """Returns the emoji's ID, relevant to the server the message was sent from."""
-        return discord.utils.get(self.guild.emojis, name=self.name).id
+        return discord.utils.get(guild.emojis, name=self.guild_name(guild=guild)).id
 
-    @property
-    def emoji(self) -> Union[str, None]:
+    def emoji(self, guild: Guild) -> Union[str, None]:
         """
         Returns the text used to render the emoji on Discord.
 
         Example return value:
         `<:blank:754688893558718495>`
         """
-        if (emoji_id := self.id) is not None:
+        if (emoji_id := self.emoji_id(guild)) is not None:
             return f"<:{self.name}:{emoji_id}>"
         else:
             return None
+
+    def guild_name(self, guild: Guild) -> str:
+        """Name of emoji in specified guild."""
+        return self.tileset.mappings[str(guild.id)][self.name]
+
+    def load_image(self, force_reload: bool = False) -> None:
+        """Retrieves emoji's png from GitHub."""
+        if not os.path.isfile(self.image_path) or force_reload:
+            urllib.request.urlretrieve(self.image_url, self.image_path)
+
+
+class Tileset:
+    """
+    Contains all data about a tileset.
+
+    Assigns itself as `tileset` property of the emojis passed in `emojis` at initialization.
+    """
+
+    def __init__(self, name: str, guild: Union[Guild, None] = None):
+        self.name = name
+        self.mappings: Dict[str, Dict[str, str]] = {}
+        self.repo_img_url: str = REPO_IMG_URLS[self.name]
+        self.emojis: Dict[str, Emoji] = {emoji_name: Emoji(name=emoji_name) for emoji_name in EMOJIS}
+
+        for emoji in self.emojis.values():  # Set attributes for all child emojis.
+            emoji.tileset: Tileset = self
+            emoji.image_path: str = f"{os.path.dirname(__file__)}/tilesets/{self.name}/{emoji.name}.png"
+            emoji.image_url: str = f"{self.repo_img_url}/dist/{emoji.name}.png"
+
+        if guild:
+            self.load_guild_mappings(guild=guild)
+
+    def load_guild_mappings(self,
+                            guild: Guild,
+                            directory: str = f"{os.path.dirname(__file__)}/tileset_config/"
+                            ) -> None:
+        """
+        Loads the tileset mappings.
+
+        If no directory is specified, it looks for the configuration file in `./tileset_config/`. The mapping is
+        relevant only to the specified guild (emojis have different IDs in different servers).
+        """
+        with open(f"{directory}{str(guild.id)}/{self.name}.json") as f:
+            self.mappings[str(guild.id)] = json.load(f)
+
+    def to_mapping(self, input_list: List[List[Emoji]], guild: Guild) -> List[List[str]]:
+        """Converts an input list with emojis to a list using the guild mapping and returns it."""
+        output: List[List[str]] = [[] for _ in range(len(input_list))]
+
+        for row in range(len(input_list)):
+            for emoji in input_list[row]:  # For the width of the character.
+                output[row].append(self.emojis[emoji.name].emoji(guild=guild))
+
+        return output
+
+    @staticmethod
+    def to_image(input_list: List[List[Emoji]]) -> Image.Image:
+        """Converts an input list with emojis to an image and returns it."""
+        tiles_wide: int = len(input_list[0])
+        tiles_high: int = len(input_list)
+
+        input_list[0][0].load_image()
+        image = Image.open(input_list[0][0].image_path)
+        tile_size = image.size[0]
+
+        output = Image.new('RGBA', (tiles_wide*tile_size, tiles_high*tile_size), (0, 0, 0, 0))
+
+        for row in range(len(input_list)):
+            for col, emoji in enumerate(input_list[row]):
+                emoji.load_image()
+                image = Image.open(emoji.image_path)
+                output.paste(image, (col*tile_size, row*tile_size))
+
+        return output
 # endregion
 
 
@@ -89,43 +164,10 @@ class Emojify(Cog):
         return "".join(output)
 
     @staticmethod
-    def _tileset(tileset_name: str,
-                 guild: Context.guild,
-                 directory: str = f"{os.path.dirname(__file__)}/tileset_config/"
-                 ) -> Dict[str, Emoji]:
-        """
-        Returns dict with tileset mappings to `Emoji` objects, of the specified tileset.
-
-        If no directory is specified, it looks for the configuration file in `./tileset_config/`. The mapping is
-        relevant only to the specified guild (emojis have different IDs in different servers).
-        """
-        with open(f"{directory}{tileset_name}.json") as f:
-            emojis = json.load(f)
-            return {key: Emoji(name=name, guild=guild) for (key, name) in emojis.items()}
-
-    @staticmethod
-    def _get_tileset_configs(guild: Context.guild,
-                             directory: str = f"{os.path.dirname(__file__)}/tileset_config/"
-                             ) -> Dict[str, Dict[str, Emoji]]:
-        """
-        Returns dict with all tileset mappings to `Emoji` objects available in the specified directory.
-
-        If no directory is specified, it looks for the configuration files in `./tileset_config/`. The mappings are
-        relevant only to the specified guild (emojis have different IDs in different servers).
-        """
-        tilesets: Dict[str, Dict[str, Emoji]] = {}
-
-        for file_path in os.listdir(directory):
-            tileset_name = file_path.split(".")[0]
-            tilesets[tileset_name] = Emojify._tileset(tileset_name=tileset_name, guild=guild)
-
-        return tilesets
-
-    @staticmethod
-    def _add_letter(emoji_list: List[List[str]],
+    def _add_letter(emoji_list: List[List[Emoji]],
                     letter: str,
                     font: Font,
-                    mapping: Dict[str, Emoji]
+                    tileset: Tileset
                     ) -> None:
         """
         Adds the emojis needed to create the specified letter to `emoji_list`.
@@ -135,35 +177,39 @@ class Emojify(Cog):
         for row in range(font.font_height):
             if not (letter_data := font.characters.get(letter)):  # If the letter can't be found in the font.
                 raise BadArgument(f"The character `{letter}` is not supported by the font {font.name}")
-            for col in range(len(letter_data[0])):
-                if not (emoji_map := mapping.get(letter_data[row][col])):  # If mapping doesn't support the emoji used.
-                    raise IncompleteConfigurationError(f"The tileset configuration file for '{font.name}' does not "
-                                                       f"specify a mapping for the emoji '{letter_data[row][col]}'.")
-                emoji_list[row].append(f"{emoji_map.emoji}")
+
+            for piece in letter_data[row]:  # For the width of the character.
+                emoji_list[row].append(tileset.emojis[piece])
 
     @staticmethod
-    def _emojify(tileset_mapping: Dict[str, Emoji], text: str, font: Font) -> List[List[str]]:
-        """Returns a list of the emojis corresponding to the input text."""
+    def _emojify(text: str, font: Font, tileset: Tileset) -> List[List[Emoji]]:
+        """Returns a list of the emoji names corresponding to the input text."""
         letter_data: List[List[str]]
-        emoji_list: List[List[str]] = [[] for _ in range(font.font_height)]
+        emoji_list: List[List[Emoji]] = [[] for _ in range(font.font_height)]
 
         if font.lowercase_only:
             text = text.lower()
 
         for letter in text:
-            Emojify._add_letter(emoji_list=emoji_list, letter=letter, font=font, mapping=tileset_mapping)
+            Emojify._add_letter(emoji_list=emoji_list, letter=letter, font=font, tileset=tileset)
 
         return emoji_list
     # endregion
 
     # region: Commands
     @command(name='emojify', aliases=('emoji',), invoke_without_command=True)
-    async def emojify_command(self, ctx: Context, tileset: str, *, text: str) -> None:
+    async def emojify_command(self, ctx: Context, tileset_name: str, *, text: str) -> None:
         """Command that responds with an emoji representation of the input string."""
-        tileset_mapping = Emojify._tileset(tileset_name=tileset, guild=ctx.guild)
-        emoji_list = Emojify._emojify(tileset_mapping=tileset_mapping, text=text, font=Font("rundi"))
+        tileset = Tileset(name=tileset_name, guild=ctx.guild)
+        output = Emojify._emojify(text=text, font=Font(name="rundi"), tileset=tileset)
+        # await ctx.send(Emojify._flatten_list(tileset.to_mapping(input_list=output, guild=ctx.guild)))
 
-        await ctx.send(self._flatten_list(emoji_list))
+        result = tileset.to_image(input_list=output)
+        data = io.BytesIO()
+        result.save(data, format='PNG')
+        data.seek(0)
+        file = discord.File(data, "output.png")
+        await ctx.send(file=file)
     # endregion
 
     # region: Error handler
@@ -179,7 +225,7 @@ class Emojify(Cog):
         if isinstance(actual_error, FileNotFoundError):
             embed.description = "We can't seem to find that tileset/font."
 
-        elif isinstance(actual_error, BadArgument):
+        if isinstance(actual_error, BadArgument):
             embed.description = str(error)
 
         elif isinstance(actual_error, IncompleteConfigurationError):
